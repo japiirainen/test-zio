f600325496bea3267168639dacc8d76319d5f11a
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-03 16:01:45 +0300

    first endpoints

--- src/main/scala/com/zio/server/Main.scala
+++ src/main/scala/com/zio/server/Main.scala
@@ -1,7 +1,56 @@
 package com.zio.server
-import scala.io.StdIn.readLine
+
+import cats.effect.{ExitCode => CatsExitCode}
+import com.zio.server.configuration.Configuration
+import com.zio.server.http.Api
+import com.zio.server.persistence.{
+  DBTransactor,
+  UserPersistence,
+  UserPersistenceService
+}
+import org.http4s.implicits._
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.middleware.CORS
+import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.console.putStrLn
+import zio.interop.catz._
 
 object Main extends App {
-  val foo = readLine("what is foo?")
-  println(foo)
+  type AppEnvironment = Configuration
+    with Clock
+    with DBTransactor
+    with UserPersistence
+
+  type AppTask[A] = RIO[AppEnvironment, A]
+
+  val appEnvironment =
+    Configuration.live >+> Blocking.live >+> UserPersistenceService.transactorLive >+> UserPersistenceService.live
+
+  override def run(args: List[String]): ZIO[ZEnv, Nothing, ExitCode] = {
+    val program: ZIO[AppEnvironment, Throwable, Unit] =
+      for {
+        _ <- UserPersistenceService.createUsersTable
+        api <- configuration.apiConfig
+        httpApp = Router[AppTask](
+          "/users" -> Api(s"${api.endpoint}/users").route
+        ).orNotFound
+
+        server <- ZIO.runtime[AppEnvironment].flatMap { implicit rts =>
+          BlazeServerBuilder[AppTask]
+            .bindHttp(api.port, api.endpoint)
+            .withHttpApp(CORS(httpApp))
+            .serve
+            .compile[AppTask, AppTask, CatsExitCode]
+            .drain
+        }
+      } yield server
+
+    program
+      .provideSomeLayer[ZEnv](appEnvironment)
+      .tapError(err => putStrLn(s"Execution failed with: $err"))
+      .exitCode
+  }
 }
